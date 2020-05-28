15f8ee35ab2d33535c33feaa873e3f592582e500
Giorgio Scorzelli <scrgiorgio@gmail.com>
2020-05-28 11:35:30 +0200

    FIxing pip install (3)

--- setup.py
+++ setup.py
@@ -1,22 +1,43 @@
-import sys,os,setuptools
+import os,sys,shutil,setuptools
 
+PROJECT_NAME="slampy"
+PROJECT_VERSION="1.0.1"
+
+this_dir=os.path.dirname(os.path.abspath(__file__))
+
+# /////////////////////////////////////////////////////////////
 def FindFiles():
-	ret=[]	
+	# this are cached directories that should not be part of OpenVisus distribution
+	shutil.rmtree('./build', ignore_errors=True)	
+	shutil.rmtree('./dist', ignore_errors=True)	
+	shutil.rmtree('./.git', ignore_errors=True)	
+	shutil.rmtree('./{}.egg-info'.format(PROJECT_NAME), ignore_errors=True)	
+
+	files=[]	
 	for dirpath, __dirnames__, filenames in os.walk("."):
 		for it in filenames:
-			ret.append(os.path.abspath(os.path.join(dirpath, it)))	
-	return ret
+			filename= os.path.abspath(os.path.join(dirpath, it))
+			
+			if "__pycache__" in filename: 
+				continue
+			
+			if os.path.splitext(filename)[1] in [".ilk",".pdb",".pyc",".pyo"]: 
+				continue
+				
+			files.append(filename)	
+			
+	return files
 
 setuptools.setup(
-	name='slampy',
-	version='1.0.1',
+	name=PROJECT_NAME,
+	version=PROJECT_VERSION,
 	url='https://github.com/sci-visus/slampy',
 	author="visus.net",
 	author_email="support@visus.net",
-	description='slampy',
-	packages=['slampy'],
-	package_dir={'slampy':'.'},
-	package_data={'slampy': FindFiles()},
+	description=PROJECT_NAME,
+	packages=[PROJECT_NAME],
+	package_dir={PROJECT_NAME:'.'},
+	package_data={PROJECT_NAME: FindFiles()},
 	install_requires=[
 		'numpy', 
 		'matplotlib',
